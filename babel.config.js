439ac2f946c124ac70a204dc6596e4f2b27340fc
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-10 11:58:05 -0300

    chore: configurando module resolver

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: '.',
+        alias: {
+          '@components': './src/components',
+          '@screen': './src/screen',
+          '@utils': './src/utils',
+          '@routes': './src/routes',
+          '@assets': './src/assets',
+          '@domain': './src/domain',
+          '@redux': './src/redux',
+        },
+      },
+    ],
+  ],
 };
