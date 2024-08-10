553fdfdb6ad3c2e90fa786beae20da5170cefb73
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-10 11:59:57 -0300

    feat: inicializando redux e o stack navigation

--- App.tsx
+++ App.tsx
@@ -1,8 +1,20 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {Provider} from 'react-redux';
+import {store} from '@redux';
+import {Router} from '@routes';
 
-function App(): React.JSX.Element {
-  return <SafeAreaView />;
+function App(): React.ReactNode {
+  return (
+    <Provider store={store}>
+      <GestureHandlerRootView>
+        <SafeAreaProvider>
+          <Router />
+        </SafeAreaProvider>
+      </GestureHandlerRootView>
+    </Provider>
+  );
 }
 
 export default App;
