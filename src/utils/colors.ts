f489421857cab6fdb9165634bb01f56a585443e2
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-10 23:19:14 -0300

    feat: criando constante para as cores do aplicativo

---
+++ src/utils/colors.ts
@@ -0,0 +1,19 @@
+const texto = '#000';
+const contraste = '#F4F4F4';
+const corDeFundo = '#FFFFFF';
+const principal = '#FEDB00';
+const linhaInput = '#23253';
+const icone = '#A2A2A7';
+const vaga = '#1FAA47';
+const marcado = '#E16364';
+
+export const cores = {
+  texto,
+  contraste,
+  corDeFundo,
+  principal,
+  linhaInput,
+  icone,
+  vaga,
+  marcado,
+};
