624f9b6aabcd51a4c0977fb55db6543cfb8ee9ba
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-10 23:18:44 -0300

    refactor: removendo propriedade

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from '@redux';
@@ -8,11 +7,9 @@ import {Router} from '@routes';
 function App(): React.ReactNode {
   return (
     <Provider store={store}>
-      <GestureHandlerRootView>
-        <SafeAreaProvider>
-          <Router />
-        </SafeAreaProvider>
-      </GestureHandlerRootView>
+      <SafeAreaProvider>
+        <Router />
+      </SafeAreaProvider>
     </Provider>
   );
 }
