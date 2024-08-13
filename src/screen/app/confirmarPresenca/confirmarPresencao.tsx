6b7d13ca57c3e9f0536ee2a5d5df15901295fb0a
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 13:49:12 -0300

    feat : adicionando tela basica somente para validar movimentacap

---
+++ src/screen/app/confirmarPresenca/confirmarPresencao.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function ConfirmarPresenca(): React.ReactElement {
+  return (
+    <>
+      <Text>confirmarPresencao</Text>
+    </>
+  );
+}
---
+++ src/screen/app/criarAgendamento/criarAgendamento.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text} from 'react-native';
+
+export function CriarAgendamento(): React.ReactElement {
+  return (
+    <>
+      <Text>CriarAgendamento</Text>
+    </>
+  );
+}
