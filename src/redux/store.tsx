017cc2be31be3d754fddef0d786f3b885443b543
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 13:43:12 -0300

    feat: configurando redux

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -1,9 +1,9 @@
 import {configureStore} from '@reduxjs/toolkit';
-import authSlice from './authSlice';
+import loginSlice from './loginSlice';
 
 const store = configureStore({
   reducer: {
-    auth: authSlice,
+    login: loginSlice,
   },
 });
 
