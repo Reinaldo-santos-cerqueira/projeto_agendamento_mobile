515efa4071a27f0762017b7f3ac974aec250d015
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 13:51:03 -0300

    feat: adicionando react query

--- App.tsx
+++ App.tsx
@@ -3,13 +3,18 @@ import {SafeAreaProvider} from 'react-native-safe-area-context';
 import {Provider} from 'react-redux';
 import {store} from '@redux';
 import {Router} from '@routes';
+import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 
 function App(): React.ReactNode {
+  const queryClient = new QueryClient();
+
   return (
     <Provider store={store}>
-      <SafeAreaProvider>
-        <Router />
-      </SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <Router />
+        </SafeAreaProvider>
+      </QueryClientProvider>
     </Provider>
   );
 }
