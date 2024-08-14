ae8f9c00419b2304588ceb2e0a7f566527bd9158
reinaldo-santos-cerqueira <kingnaldoreinaldo@gmail.com>
2024-08-13 22:45:11 -0300

    feat: novas propiedades

--- src/redux/loginSlice.tsx
+++ src/redux/loginSlice.tsx
@@ -1,22 +1,30 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {createSlice, PayloadAction} from '@reduxjs/toolkit';
 
 interface LoginState {
   logged: boolean;
+  token: string;
+  tipo: 'P' | 'F' | '';
 }
 
 const initialState: LoginState = {
   logged: false,
+  token: '',
+  tipo: '',
 };
 
 const loginSlice = createSlice({
   name: 'login',
   initialState,
   reducers: {
-    login: state => {
+    login: (state, action: PayloadAction<{token: string; tipo: 'P' | 'F'}>) => {
       state.logged = true;
+      state.token = action.payload.token;
+      state.tipo = action.payload.tipo;
     },
     logout: state => {
       state.logged = false;
+      state.token = '';
+      state.tipo = '';
     },
   },
 });
